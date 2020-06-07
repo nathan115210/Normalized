4efc602c79f2e8a48d801e8368693b1e8df26b2b
Zhao Hongyu <nathan_115210@hotmail.com>
2020-06-07 17:12:42 +0300

    doc(tabs): add tabs storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import Tabs from "../src/components/Tabs/Tabs";
+import TabItem from "../src/components/Tabs/TabItem";
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Tabs type="card">
+        <TabItem label="tab1">content1</TabItem>
+        <TabItem label="tab2">content2</TabItem>
+        <TabItem label="disabled" disabled>
+          content3
+        </TabItem>
+      </Tabs>
+    </div>
+  );
 }
 
 export default App;
