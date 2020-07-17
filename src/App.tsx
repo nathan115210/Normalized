596447453339283389df11cab33e9e543d8d9c60
Zhao Hongyu <nathan_115210@hotmail.com>
2020-07-17 07:25:32 +0300

    feat(subMenu):add testing submenus into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import SubMenu from "./components/Menu/SubMenu";
+import Menu from "./components/Menu/Menu";
+import MenuItem from "./components/Menu/MenuItem";
+library.add(fas);
 
 function App() {
-  return <div className="App">Hello world</div>;
+  const element = (
+    <Menu>
+      <MenuItem>active</MenuItem>
+      <MenuItem disabled>disabled</MenuItem>
+      <MenuItem>link 3</MenuItem>
+      <SubMenu title="dropdown">
+        <MenuItem>drop 1</MenuItem>
+        <MenuItem>drop 1</MenuItem>
+        <MenuItem>drop 1</MenuItem>
+        <MenuItem>drop 1</MenuItem>
+        <MenuItem>drop 1</MenuItem>
+      </SubMenu>
+    </Menu>
+  );
+  return <div className="App">{element}</div>;
 }
 
 export default App;
