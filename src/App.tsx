35781481536314d5519109af90e3f7063cad1c86
Zhao Hongyu <nathan_115210@hotmail.com>
2020-07-23 13:31:14 +0300

    chore: update testing App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import SubMenu from "./components/Menu/SubMenu";
 import Menu from "./components/Menu/Menu";
 import MenuItem from "./components/Menu/MenuItem";
+import Button from "./components/Button/Button";
+import Transition from "./components/Transition/Transition";
 library.add(fas);
 
-function App() {
-  const element = (
-    <Menu>
-      <MenuItem>active</MenuItem>
-      <MenuItem disabled>disabled</MenuItem>
-      <MenuItem>link 3</MenuItem>
-      <SubMenu title="dropdown">
-        <MenuItem>drop 1</MenuItem>
-        <MenuItem>drop 1</MenuItem>
-        <MenuItem>drop 1</MenuItem>
-        <MenuItem>drop 1</MenuItem>
-        <MenuItem>drop 1</MenuItem>
-      </SubMenu>
-    </Menu>
+const App: React.FC = () => {
+  const [show, setShow] = useState(false);
+  console.log("show", show);
+
+  const menu = (
+    <>
+      <Menu>
+        <MenuItem>active</MenuItem>
+        <MenuItem disabled>disabled</MenuItem>
+        <MenuItem>link 3</MenuItem>
+        <SubMenu title="dropdown" animation="zoom-in-bottom">
+          <MenuItem>drop 1</MenuItem>
+          <MenuItem>drop 1</MenuItem>
+          <MenuItem>drop 1</MenuItem>
+          <MenuItem>drop 1</MenuItem>
+          <MenuItem>drop 1</MenuItem>
+        </SubMenu>
+      </Menu>
+    </>
   );
-  return <div className="App">{element}</div>;
-}
+  const button = (
+    <>
+      <Button size="lg" onClick={() => setShow(!show)}>
+        big
+      </Button>
+      <Transition in={show} timeout={300} animation="zoom-in-left" wrapper>
+        <Button btnType="danger" size="lg" onClick={() => setShow(!show)}>
+          big
+        </Button>
+      </Transition>
+    </>
+  );
+  return (
+    <div className="App">
+      {menu}
+      {button}
+    </div>
+  );
+};
 
 export default App;
