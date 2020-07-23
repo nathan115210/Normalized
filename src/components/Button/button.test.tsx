f13119895ce65ebaf37d31e1eecf502bb90f1e4b
Zhao Hongyu <nathan_115210@hotmail.com>
2020-07-23 13:40:38 +0300

    fix(Button): fix the test className

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -33,7 +33,7 @@ describe("test Button component", () => {
     const wrapper = render(<Button {...testProps}>Button</Button>);
     const element = wrapper.getByText("Button");
     expect(element).toBeInTheDocument();
-    expect(element).toHaveClass("btn-primary btn-large testClassName");
+    expect(element).toHaveClass("btn-primary btn-lg testClassName");
   });
   it("should render a link when the btnType is link and href is provided", () => {
     const wrapper = render(
