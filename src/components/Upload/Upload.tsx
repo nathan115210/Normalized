04dd048d76a8fcbda123ff765b8b13d160eca4ce
Zhao Hongyu <nathan_115210@hotmail.com>
2020-09-25 09:18:06 +0300

    feat(Upload): show the upload results on frontEnd

--- src/components/Upload/Upload.tsx
+++ src/components/Upload/Upload.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useRef, ChangeEvent, useState } from "react";
 import axios from "axios";
-
+import UploadList from "./UploadList";
 import Button from "../Button/Button";
 
 export type UploadFileStatus = "ready" | "uploading" | "success" | "error";
@@ -18,24 +18,29 @@ export interface UploadFile {
 
 export interface UploadProps {
   action: string;
+  defaultFileList?: UploadFile[];
   beforeUpload?: (file: File) => boolean | Promise<File>;
+  //TODO: change the File type to the UploadFile
   onProgress?: (percentage: number, file: File) => void;
   onSuccess?: (data: any, file: File) => void;
   onError?: (err: any, file: File) => void;
   onChange?: (file: File) => void;
+  onRemove?: (file: UploadFile) => void;
 }
 
 export const Upload: FC<UploadProps> = (props) => {
   const {
     action,
+    defaultFileList,
     beforeUpload,
     onProgress,
     onSuccess,
     onError,
     onChange,
+    onRemove,
   } = props;
   const fileInput = useRef<HTMLInputElement>(null);
-  const [fileList, setFileList] = useState<UploadFile[]>([]);
+  const [fileList, setFileList] = useState<UploadFile[]>(defaultFileList || []);
   const updateFileList = (
     updateFile: UploadFile,
     updateObject: Partial<UploadFile>
@@ -118,6 +123,14 @@ export const Upload: FC<UploadProps> = (props) => {
       fileInput.current.value = "";
     }
   };
+  const handleRemove = (file: UploadFile) => {
+    setFileList((prevList) => {
+      return prevList.filter((item) => item.uid !== file.uid);
+    });
+    if (onRemove) {
+      onRemove(file);
+    }
+  };
   const uploadFiles = (files: FileList) => {
     let postFiles = Array.from(files);
     postFiles.forEach((file) => {
@@ -148,6 +161,7 @@ export const Upload: FC<UploadProps> = (props) => {
         onChange={handleFileChange}
         type="file"
       />
+      <UploadList fileList={fileList} onRemove={handleRemove} />
     </div>
   );
 };
--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-import { Upload } from "./Upload";
+import { Upload, UploadFile } from "./Upload";
 
+const defaultFileList: UploadFile[] = [
+  {
+    uid: "123",
+    size: 1234,
+    name: "hello.md",
+    status: "uploading",
+    percent: 30,
+  },
+  { uid: "122", size: 1234, name: "xyz.md", status: "success", percent: 30 },
+  { uid: "121", size: 1234, name: "eyiha.md", status: "error", percent: 30 },
+];
 const checkFileZSize = (file: File) => {
   if (Math.round(file.size / 1024) > 20000) {
     alert("file too big");
@@ -19,6 +30,8 @@ const SimpleUpload = () => {
     <Upload
       action="https://run.mocky.io/v3/0278e822-1a48-4dcf-bef9-3a328ad6de79"
       onChange={action("changed")}
+      defaultFileList={defaultFileList}
+      onRemove={action("removed")}
     />
   );
 };
